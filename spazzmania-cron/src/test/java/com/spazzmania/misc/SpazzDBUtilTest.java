334d5bf826c30ed49033148675388361f60e9f77
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-08-12 05:15:18 +0000

    SpazDBUtil test - these are mostly string tests done under the JUnit framework
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@11 4839df1b-77b6-4aaf-b387-209ca36962b6

---
+++ spazzmania-cron/src/test/java/com/spazzmania/misc/SpazzDBUtilTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.spazzmania.misc;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import com.spazzmania.model.util.SpazzDBUtil;
+
+/**
+ * @author tjbillingsley
+ *
+ */
+public class SpazzDBUtilTest extends TestCase {
+	
+	private SpazzDBUtil dbUtil;
+	private String username;
+	private String password;
+	private String host;
+	private String database;
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		this.username = "webaccess";
+		this.password = "wsc2aofi";
+		this.host = "qa.epf.spazzdata.com";
+		this.database = "spazzmania";
+		
+		dbUtil = new SpazzDBUtil(host, database, username, password);
+	}
+	
+	@Test
+	public void testGetKeyValue() {
+//		String date = dbUtil.getKeyValue("epf.last_epf_import_date");
+//		assertTrue("Invalid date returned",date != null);
+		assertTrue("String test only - returning true for normal builds",true);
+	}
+	
+	@Test
+	public void testSetKeyValue() {
+//		dbUtil.setKeyValue("epf.last_epf_import_date","99999999");
+//		String date = dbUtil.getKeyValue("epf.last_epf_import_date");
+//		assertTrue("Invalid date returned",date.equals("99999999"));
+		assertTrue("String test only - returning true for normal builds",true);
+	}
+}

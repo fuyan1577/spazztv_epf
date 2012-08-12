ec5e6e7d77c195ebf782d8e6366a3807a6204a74
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-08-12 21:21:27 +0000

    Moved early development test objects to archive folder
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@21 4839df1b-77b6-4aaf-b387-209ca36962b6

---
+++ spazzmania-cron/src/test/java/com/spazzmania/archive/JsoupTest.java
@@ -0,0 +1,78 @@
+package com.spazzmania.archive;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+import org.apache.commons.codec.binary.Base64;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
+
+public class JsoupTest {
+
+	@Test
+	public void test() {
+		try {
+			String username = "D1g1talmunch";
+			String password = "ad4478914af590e2d53565c910105bf3";
+			String login = username + ":" + password;
+			String base64login = new String(Base64.encodeBase64(login
+					.getBytes()));
+
+			Document document = Jsoup
+					.connect("http://feeds.itunes.apple.com/feeds/epf/v3/full")
+					.header("Authorization", "Basic " + base64login).get();
+
+			Object[] hrefs = document.select("a").toArray();
+			for (Object href : hrefs) {
+				String h = ((Element) href).attr("href");
+				if (Pattern.matches("\\d+\\/", h)) {
+					System.out.println(h);
+				}
+			}
+
+			document = Jsoup
+					.connect(
+							"http://feeds.itunes.apple.com/feeds/epf/v3/full/current/incremental/20120809/")
+					.header("Authorization", "Basic " + base64login).get();
+
+			Object[] shrefs = document.select("a").toArray();
+			for (Object href : shrefs) {
+				String h = ((Element) href).attr("href");
+				if (Pattern.matches("\\d+\\/", h)) {
+					System.out.println(h);
+				}
+			}
+
+			for (Object href : shrefs) {
+				String h = ((Element) href).attr("href");
+				if (Pattern.matches("[a-z]+\\d+\\.tbz", h)) {
+					System.out.println(h);
+				}
+			}
+
+			// System.out.println(document.toString());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testDate() {
+		DateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+		Date date;
+		try {
+			date = (Date) formatter.parse("20120801/");
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println(date.toString());
+		
+	}
+}
---
+++ spazzmania-cron/src/test/java/com/spazzmania/archive/Log4jMDCTest1.java
@@ -0,0 +1,57 @@
+package com.spazzmania.archive;
+
+import java.io.IOException;
+
+import org.apache.log4j.FileAppender;
+
+public class Log4jMDCTest1 implements Runnable {
+
+	private Integer threadNumber;
+	private String threadName;
+	private String jobName;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 1; i <= 2; i++) {
+			Runnable runnable = new Log4jMDCTest1(new Integer(i));
+			(new Thread(runnable)).start();
+		}
+	}
+
+	public Log4jMDCTest1(Integer threadNumber) {
+		this.threadNumber = threadNumber;
+		threadName = "MDCThread-" + this.threadNumber.toString();
+		jobName = "Log4jMDCTest-" + this.threadNumber.toString();
+
+		//SimpleLayout layout = new SimpleLayout();
+		org.apache.log4j.PatternLayout layout = new org.apache.log4j.PatternLayout("%-5p | %d %t %c: %m%n");
+
+		FileAppender appender = null;
+		try {
+			appender = new FileAppender(layout, "log/" + threadName + ".log",
+					false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// logger.setLevel((Level) Level.DEBUG);
+		
+		org.apache.log4j.Logger logger = org.apache.log4j.Logger
+				.getLogger(jobName);
+		//logger.setLevel((Level) Level.INFO);
+		logger.addAppender(appender);
+	}
+
+	public void run() {
+		Thread thread = Thread.currentThread();
+		thread.setName(threadName);
+
+		// Logger logger = LoggerFactory.getLogger(jobName);
+		org.apache.log4j.Logger logger = org.apache.log4j.Logger
+				.getLogger(jobName);
+		for (int i = 0; i < 10; i++) {
+			logger.info("Hello from MDC Test Step: " + jobName);
+		}
+	}
+}

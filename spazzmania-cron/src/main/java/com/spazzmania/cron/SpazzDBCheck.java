b820ef6b6a6c177fbb1ce8f8e2bc0ecdf05c006c
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-08-13 00:52:25 +0000

    SpazzDBCheck - Added a simple command line utility to verify the jar was build correctly and that it can talk to the database.
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@24 4839df1b-77b6-4aaf-b387-209ca36962b6

---
+++ spazzmania-cron/src/main/java/com/spazzmania/cron/SpazzDBCheck.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.spazzmania.cron;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import com.spazzmania.epf.EPFDataDownloadUtil;
+import com.spazzmania.model.util.SpazzDBUtil;
+
+/**
+ * @author tjbillingsley
+ * 
+ */
+public class SpazzDBCheck {
+
+	@Option(name = "--username", metaVar = "<username>")
+	private String username;
+
+	@Option(name = "--password", metaVar = "<password>")
+	private String password;
+
+	@Option(name = "--host", metaVar = "<host>")
+	private String host;
+
+	public void run() {
+		SpazzDBUtil dbUtil = new SpazzDBUtil(host, "spazzmania", username,
+				password);
+		String lastDownloadDate = dbUtil
+				.getKeyValue(EPFDataDownloadUtil.EPF_LAST_DOWNLOAD_DATE);
+		if (lastDownloadDate != null) {
+			System.out.println("Looks good...");
+		}
+		System.out.println(String.format("System Key %s = %s",
+				EPFDataDownloadUtil.EPF_LAST_DOWNLOAD_DATE, lastDownloadDate));
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length != 6) {
+			throw new RuntimeException(
+					"SpazzDBCheck --host=SPAZZDBURL --username=USERNAME --password=PASSWORD");
+		}
+		final SpazzDBCheck check = new SpazzDBCheck();
+	    final CmdLineParser parser = new CmdLineParser(check);
+	    try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e) {
+			throw new RuntimeException(e);
+		}
+	    System.out.println("SpazzDBCheck - Checking the connection to the Spazzmania DB...");
+		check.run();
+	}
+}

ae65e1d4992d741ccc43a387d6f78aae0b86e13a
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-08-20 02:50:39 +0000

    Added a loadScriptList which loads a single directory list of SQL scripts as a sorted list
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@53 4839df1b-77b6-4aaf-b387-209ca36962b6

--- spazzmania-cron/src/main/java/com/spazzmania/sql/util/SQLResourceUtil.java
+++ spazzmania-cron/src/main/java/com/spazzmania/sql/util/SQLResourceUtil.java
@@ -3,8 +3,22 @@
  */
 package com.spazzmania.sql.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Pattern;
 
 /**
  * @author Thomas Billingsley
@@ -35,4 +49,66 @@ public class SQLResourceUtil {
 
 		return new String(buffer);
 	}
+
+	/**
+	 * List directory contents for a resource folder. Not recursive. This is
+	 * basically a brute-force implementation. Works for regular files and also
+	 * JARs.
+	 * 
+	 * @author Greg Briggs
+	 * @param path
+	 *            Should end with "/", but not start with one.
+	 * @return Just the name of each member item, not the full paths.
+	 * @throws URISyntaxException
+	 * @throws IOException
+	 */
+	public synchronized static List<String> loadScriptList(String path) {
+		try {
+			URL dirURL = SQLResourceUtil.class.getResource(path);
+			if (dirURL != null && dirURL.getProtocol().equals("file")) {
+				/* A file path: easy enough */
+
+				Set<String> resultSet = new TreeSet<String>();
+				File fileDir = new File(dirURL.toURI());
+				for (int i = 0; i < fileDir.list().length; i++) {
+					String entry = fileDir.list()[i];
+					if (Pattern.matches(".+\\.sql",entry)) {
+						resultSet.add(entry);
+					}
+				}
+				return Arrays.asList(resultSet.toArray(new String[resultSet.size()]));
+			}
+
+			if (dirURL == null) {
+				return null;
+			}
+
+			if (dirURL.getProtocol().equals("jar")) {
+				/* A JAR path */
+				String jarPath = dirURL.getPath().substring(5,
+						dirURL.getPath().indexOf("!")); // strip out only the
+														// JAR
+														// file
+				JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+				Enumeration<JarEntry> entries = jar.entries(); // gives ALL
+																// entries
+																// in jar
+				Set<String> resultSet = new TreeSet<String>(); // avoid duplicates
+															// in
+															// case it is a
+															// subdirectory
+				while (entries.hasMoreElements()) {
+					String entry = entries.nextElement().getName();
+					if (Pattern.matches(".+\\.sql",entry)) {
+						resultSet.add(entry);
+					}
+				}
+				return Arrays.asList(resultSet.toArray(new String[resultSet.size()]));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return new ArrayList<String>();
+	}
+	
 }
--- spazzmania-cron/src/test/java/com/spazzmania/cron/SQLResourceUtilTest.java
+++ spazzmania-cron/src/test/java/com/spazzmania/cron/SQLResourceUtilTest.java
@@ -1,5 +1,7 @@
 package com.spazzmania.cron;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +21,19 @@ public class SQLResourceUtilTest {
 						+ EPF2SpazzDataUpdateUtil.UPDATE_GAMES);
 
 		Assert.assertNotNull("SQL Resource Not Loaded", script);
+	}
+
+	@Test
+	public void testLoadScriptList() {
+		List<String> fileList = SQLResourceUtil
+				.loadScriptList(EPF2SpazzDataUpdateUtil.EPF_UTIL_SQL_PATH);
+		
+		Assert.assertTrue("EPF Update SQL resource list was empty",fileList.size() > 0);
+		
+		fileList = SQLResourceUtil
+				.loadScriptList(SpazzmaniaCustomGenresUtil.GENRE_SQL_PATH);
+		
+		Assert.assertTrue("Spazz Custom Genre SQL resource list was empty",fileList.size() > 0);
 	}
 
 }

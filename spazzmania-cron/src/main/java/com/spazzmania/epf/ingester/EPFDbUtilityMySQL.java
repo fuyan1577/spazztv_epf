af5dfc7aff65bd79cf697ddc76e31b8cdc6f2159
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-08-27 05:25:58 +0000

    Created a stubbed out MySQL implementation of EPFDbUtility
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@61 4839df1b-77b6-4aaf-b387-209ca36962b6

---
+++ spazzmania-cron/src/main/java/com/spazzmania/epf/ingester/EPFDbUtilityMySQL.java
@@ -0,0 +1,38 @@
+package com.spazzmania.epf.ingester;
+
+import java.util.LinkedHashMap;
+
+public class EPFDbUtilityMySQL implements EPFDbUtility {
+
+	@Override
+	public void initImport(EPFImportType importType, String tableName,
+			long numberOfRows) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createTable(LinkedHashMap<String, String> columnsAndTypes) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPrimaryKey(String columnName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertRow(String[] rowData) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void finalizeImport() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

3f6ef9115640d6530f72882de87528a9a822ee02
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-09-09 03:48:34 +0000

    Moved the EPF Feed connectors to com.spazzmania.epf.feed
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@82 4839df1b-77b6-4aaf-b387-209ca36962b6

---
+++ spazzmania-cron/src/test/java/com/spazzmania/epf/feed/EPFConnectorImplTest.java
@@ -0,0 +1,48 @@
+package com.spazzmania.epf.feed;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.spazzmania.epf.feed.EPFConnectorImpl;
+
+/**
+ * EPFConnectorImplTest - This is a End to End String test of the
+ * EPFConnectorImpl, an object which download data from Apple's EPF site.
+ * 
+ * <p>For build testing, the String test methods were commented
+ * out. Only the <code>getDownloadFilename()</code> was left uncommented.
+ * 
+ * @author tjbillingsley
+ * 
+ */
+public class EPFConnectorImplTest {
+
+	private EPFConnectorImpl epfConnector;
+	private String testDownloadURL;
+	private String testFilename;
+
+	@Before
+	public void setUp() throws Exception {
+		epfConnector = new EPFConnectorImpl();
+		testDownloadURL = "http://feeds.itunes.apple.com/feeds/epf/v3/full/20120808/popularity20120808.tbz";
+		testFilename = "popularity20120808.tbz";
+	}
+
+	@Test
+	public void testDownloadUrl() {
+		// This is a string test and should be uncommented only when running the
+		// test
+		// epfConnector.setShowProgress(true);
+		// epfConnector.downloadFileFromUrl(testDownloadURL, "target/");
+		assertTrue("String testing object only - returning true", true);
+	}
+
+	@Test
+	public void testGetDownloadFilename() {
+		String filename = epfConnector.getDownloadFilename(testDownloadURL);
+		assertTrue(String.format("Expecting %s, filename returned was %s",
+				testFilename, filename), filename.equals(testFilename));
+	}
+}

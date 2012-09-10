44b18d2a3567b97f17d15de2ced32404ac8a58c3
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-09-10 06:22:59 +0000

    Refactored EPFDbConnector eliminating need for EPFDbConnectorBoneCP
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@93 4839df1b-77b6-4aaf-b387-209ca36962b6

--- spazzmania-cron/src/main/java/com/spazzmania/epf/ingester/EPFDbConnector.java
+++ spazzmania-cron/src/main/java/com/spazzmania/epf/ingester/EPFDbConnector.java
@@ -1,146 +1,62 @@
 package com.spazzmania.epf.ingester;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.jolbox.bonecp.BoneCP;
+import com.jolbox.bonecp.BoneCPConfig;
 
 /**
- * This is an abstract object for wrapping a JDBC Connection Pool. The defined
- * methods load the JSON formatted configuration.
+ * This is an object for wrapping a JDBC Connection Pool. The current
+ * implementation uses BoneCP.
  * 
  * <p/>
- * The logic is broken into the two following steps:
- * <ol>
- * <li/>Load the JSON Configuration for the database connection pool -
- * <i>Implemented in this class</i>
- * <li/>Return Connection object from the database connection pool -
- * <i>Implemented by child classes</i>
- * </ol>
+ * The provided EPFDbConfig object is used to set up the Connection Pool object.
  * 
  * <p/>
- * The configuration is loaded from an EPFDbConnector.json configuration file
- * located in the local config path. The configuration file designates the JDBC
- * Class, schema, username and password to be used by the database connection
- * pool.
- * 
+ * Classes call the <i>getConnection()</i> method to retrieve a connection. The
+ * connections are released back into the pool by calling the <i>connection.close()</i>
+ * method.
  * <p/>
- * The second function of this object is to open the database connection pool
- * and return connections through the <i>getConnection()</i> method.
- * 
- * <p/>
- * Implementing classes which extend this class load the actual database
- * connection pool object.
+ * A call should be made to <i>closeConnectionPool()</i> at the end of database processing.
  * 
  * @author Thomas Billingsley
  * 
  */
-public abstract class EPFDbConnector {
+public class EPFDbConnector {
 
-	public static String CONNECTION_POOL = "dbConnectionPool";
-	public static String JDBC_DRIVER_CLASS = "dbJdbcDriverClass";
-	public static String JDBC_DEFAULT_CATALOG = "dbDefaultCatalog";
-	public static String JDBC_MIN_CONNECTIONS = "dbMinConnections";
-	public static String JDBC_MAX_CONNECTIONS = "dbMaxConnections";
-	public static String JDBC_URL = "dbJdbcUrl";
-	public static String JDBC_USER = "dbUser";
-	public static String JDBC_PASSWORD = "dbPassword";
 	public static Long DEFAULT_MIN_CONNECTIONS = 5L;
 	public static Long DEFAULT_MAX_CONNECTIONS = 20L;
 
-	public class DBConfig {
-		public String jdbcDriverClass;
-		public String defaultCatalog;
-		public String jdbcUrl;
-		public String username;
-		public String password;
-		public Long minConnections;
-		public Long maxConnections;
+	private BoneCP connectionPool;
+	private EPFDbConfig dbConfig;
+
+	public EPFDbConnector(EPFDbConfig dbConfig) {
+		this.dbConfig = dbConfig;
 	}
 
-	private DBConfig dbConfig;
-
-	public EPFDbConnector() {
-		dbConfig = new DBConfig();
-	}
-
-	public EPFDbConnector(String configFilePath) throws IOException {
-		dbConfig = new DBConfig();
-		String configJson = loadConfigFile(configFilePath);
-		parseConfiguration(configJson);
-	}
-
-	public abstract void openConnectionPool(String configPath)
-			throws IOException, ClassNotFoundException, SQLException;
-
-	public abstract void closeConnectionPool();
-
-	public abstract Connection getConnection() throws SQLException;
-
-	public DBConfig getDBConfig() {
+	public EPFDbConfig getEPFDbConfig() {
 		return dbConfig;
 	}
 
-	public void parseConfiguration(String configJson) {
-		JSONParser parser = new JSONParser();
-		JSONObject connPoolObject;
-		try {
-			JSONObject json = (JSONObject) parser.parse(configJson);
-			connPoolObject = (JSONObject) json.get(CONNECTION_POOL);
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
-
-		// Required Values
-		dbConfig.jdbcDriverClass = verifyString(connPoolObject,
-				JDBC_DRIVER_CLASS);
-		dbConfig.jdbcUrl = verifyString(connPoolObject, JDBC_URL);
-		dbConfig.username = verifyString(connPoolObject, JDBC_USER);
-		dbConfig.password = verifyString(connPoolObject, JDBC_PASSWORD);
-
-		// Optional values
-		dbConfig.defaultCatalog = (String) connPoolObject
-				.get(JDBC_DEFAULT_CATALOG);
-		dbConfig.minConnections = verifyLongDefault(connPoolObject,
-				JDBC_MIN_CONNECTIONS, DEFAULT_MIN_CONNECTIONS);
-		dbConfig.maxConnections = verifyLongDefault(connPoolObject,
-				JDBC_MAX_CONNECTIONS, DEFAULT_MAX_CONNECTIONS);
+	public void openConnectionPool(String configPath) throws IOException,
+			ClassNotFoundException, SQLException {
+		EPFDbConfig dbConfig = this.getEPFDbConfig();
+		BoneCPConfig config = new BoneCPConfig();
+		Class.forName(dbConfig.getJdbcDriverClass());
+		config.setJdbcUrl(dbConfig.getJdbcUrl());
+		config.setDefaultCatalog(dbConfig.getDefaultCatalog());
+		config.setUsername(dbConfig.getUsername());
+		config.setPassword(dbConfig.getPassword());
+		connectionPool = new BoneCP(config);
 	}
 
-	private String verifyString(JSONObject connPoolObject, String key) {
-		if (connPoolObject.get(key) == null) {
-			throw new RuntimeException(String.format(
-					"Missing EPFDbConnector Parameter: %s", key));
-		}
-		return (String) connPoolObject.get(key);
+	public void closeConnectionPool() {
+		connectionPool.close();
 	}
 
-	private Long verifyLongDefault(JSONObject connPoolObject, String key,
-			Long defaultValue) {
-		if (connPoolObject.get(key) == null) {
-			return defaultValue;
-		}
-		return (Long) connPoolObject.get(key);
-	}
-
-	private String loadConfigFile(String configFilePath) throws IOException {
-		FileInputStream stream = new FileInputStream(new File(configFilePath));
-		try {
-			FileChannel fc = stream.getChannel();
-			MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0,
-					fc.size());
-			/* Instead of using default, pass in a decoder. */
-			return Charset.defaultCharset().decode(bb).toString();
-		} finally {
-			stream.close();
-		}
+	public Connection getConnection() throws SQLException {
+		return connectionPool.getConnection();
 	}
 }

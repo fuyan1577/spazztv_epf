e18a7a2c174b015bea8d5c07ef5a0f5105232ad0
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-09-10 06:34:57 +0000

    Added the class EPFImportTask as the worker thread classes of the execution pool
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@100 4839df1b-77b6-4aaf-b387-209ca36962b6

---
+++ spazzmania-cron/src/main/java/com/spazzmania/epf/ingester/EPFImportTask.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.spazzmania.epf.ingester;
+
+/**
+ * 
+ * @author Thomas Billingsley
+ *
+ */
+public class EPFImportTask implements Runnable {
+
+	public EPFImportTask(EPFParser parser, EPFDbConnector connector) {
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+}

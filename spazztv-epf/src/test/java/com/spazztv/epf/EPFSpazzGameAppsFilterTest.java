1f54c1588f9158248e163a54311277572cd7e88f
tjbillingsley <tjbillingsley@4839df1b-77b6-4aaf-b387-209ca36962b6>
2012-12-25 21:53:50 +0000

    Updated the comments in the test object
    git-svn-id: http://www.spazzdev.com/spazz_serverj/trunk@195 4839df1b-77b6-4aaf-b387-209ca36962b6

--- spazztv-epf/src/test/java/com/spazztv/epf/EPFSpazzGameAppsFilterTest.java
+++ spazztv-epf/src/test/java/com/spazztv/epf/EPFSpazzGameAppsFilterTest.java
@@ -6,10 +6,8 @@ import java.util.List;
 
 import junit.framework.Assert;
 
-import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
-import org.powermock.api.easymock.PowerMock;
 
 /**
  * This a JUnit test which actually loads small test files to verify the
@@ -26,9 +24,6 @@ public class EPFSpazzGameAppsFilterTest {
 
 	private static String EPF_DATA_GENRE_APPLICATION = "testdata/epf_files/genre_application";
 	private static String EPF_DATA_DIRECTORY = "testdata/epf_files";
-
-	private String businessGenre = "6000";
-	private String gamesGenre = "6014";
 
 	@Before
 	public void setUp() throws Exception {
@@ -60,73 +55,6 @@ public class EPFSpazzGameAppsFilterTest {
 				"Invalid diretory expecting %s, actual %s",
 				directoryOnly.getName(), actualDirectory.getName()),
 				directoryOnly, actualDirectory);
-	}
-
-	@Test
-	public void testLoadApplicationIds() throws Exception {
-		EPFImportTranslator mockImportTranslator = EasyMock
-				.createMock(EPFImportTranslator.class);
-		SimpleEPFFileReader mockFileReader = EasyMock
-				.createMock(SimpleEPFFileReader.class);
-		File mockGenreApplicationFile = EasyMock.createMock(File.class);
-
-		String expectedGenreAppFile = directoryOnly.getPath()
-				+ File.separatorChar
-				+ EPFSpazzGameAppsFilter.GENRE_APPLICATION_FILE;
-
-		// These mocks are having no effect - the objects are being
-		// instantiated.
-		PowerMock.expectNew(File.class, expectedGenreAppFile).andReturn(
-				mockGenreApplicationFile);
-		PowerMock.replay(File.class);
-
-		PowerMock.expectNew(SimpleEPFFileReader.class, EasyMock.anyObject(),
-				EasyMock.anyObject(), EasyMock.anyObject()).andReturn(
-				mockFileReader);
-		PowerMock.replay(SimpleEPFFileReader.class);
-
-		PowerMock.expectNew(EPFImportTranslator.class, EasyMock.anyObject())
-				.andReturn(mockImportTranslator);
-		PowerMock.replay(EPFImportTranslator.class);
-
-		EasyMock.reset(mockGenreApplicationFile);
-		EasyMock.replay(mockGenreApplicationFile);
-
-		EasyMock.reset(mockFileReader);
-		mockFileReader.getRecordsWritten();
-		EasyMock.expectLastCall().andReturn(4L).times(1);
-		EasyMock.replay(mockFileReader);
-
-		EasyMock.reset(mockImportTranslator);
-		mockImportTranslator.hasNextRecord();
-		EasyMock.expectLastCall().andReturn(true).times(4);
-		EasyMock.expectLastCall().andReturn(false).times(1);
-		mockImportTranslator.nextRecord();
-		EasyMock.expectLastCall()
-				.andReturn(getMockGenreAppRecord(gamesGenre, "11111111"))
-				.times(1);
-		EasyMock.expectLastCall()
-				.andReturn(getMockGenreAppRecord(businessGenre, "22222222"))
-				.times(1);
-		EasyMock.expectLastCall()
-				.andReturn(getMockGenreAppRecord(gamesGenre, "33333333"))
-				.times(1);
-		EasyMock.expectLastCall()
-				.andReturn(getMockGenreAppRecord(businessGenre, "44444444"))
-				.times(1);
-		EasyMock.replay(mockImportTranslator);
-
-		appsFilter = EPFSpazzGameAppsFilter.getInstance(directoryOnly);
-
-		appsFilter.loadApplicationIds();
-
-		// PowerMock.verify(SimpleEPFFileReader.class);
-		// PowerMock.verify(EPFImportTranslator.class);
-		// EasyMock.verify(mockImportTranslator);
-	}
-
-	private List<String> getMockGenreAppRecord(String genreId, String appId) {
-		return null;
 	}
 
 	@Test
